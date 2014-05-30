a2dc87419cff622f6f84ad34371772f7bc1722d8
Simon Zheng <xiaomengzheng@gmail.com>
2014-05-29 17:30:25 -0700

    add login register template and view handling

--- app.js
+++ app.js
@@ -10,4 +10,48 @@ app.get('/', function(req, res){
 	res.render("index.jade", {layout:false});
 });
 
+app.get('/account/authenticated', function(req, res) {
+	if ( req.session.loggedIn ) {
+		res.send(200);
+	} else {
+		res.send(401);
+	}
+});
+
+app.post('/register', function(req, res) {
+	var firstName = req.param('firstName', '');
+	var lastName = req.param('lastName', '');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+	
+	if ( null == email || null == password ) {
+		res.send(400);
+		return;
+	}
+	
+	Account.register(email, password, firstName, lastName);
+	res.send(200);
+});
+
+app.post('/login', function(req, res) {
+	console.log('login request');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+	
+	if ( null == email || email.length < 1 
+		|| null == password || password.length < 1 ) {
+		res.send(400);
+		return;
+	}
+	
+	Account.login(email, password, function(success) {
+		if ( !success ) {
+			res.send(401);
+			return;
+		}
+		console.log('login was successful');
+		res.send(200);
+	});
+});
+
 app.listen(8080);
